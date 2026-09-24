2fcf8ba9d3f62a397ae389399e977080f55da5d7
Mardakhay Ravinov <alfredrockankeur@gmail.com>
2026-09-24 14:30:40 +0400

    Add PWA manifest and service worker

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import App from './App.tsx'
 import './index.css'
+import { registerServiceWorker } from './registerServiceWorker'
 
 const queryClient = new QueryClient()
+
+registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
